8839009029f6e9f3e4e963dbdab09d66274c7e28
Jacob W <jacob.williams@nih.gov>
2023-08-17 11:34:26 -0400

    Properly remove sort.temp

--- UKB_QC/QC_UKBB_bcftools_concat.sh
+++ UKB_QC/QC_UKBB_bcftools_concat.sh
@@ -2,7 +2,7 @@
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --time=144:00:00
-#SBATCH --array=22
+#SBATCH --array=20-21
 #SBATCH --mem-per-cpu=10G
 
 module load samtools
@@ -11,4 +11,4 @@ OUTPUT_PATH=/data/williamsjacr/UKB_WES_lipids/Data/pVCF
 
 ls ${OUTPUT_PATH}/chr${SLURM_ARRAY_TASK_ID}/ukb23156_c${SLURM_ARRAY_TASK_ID}_b*_v1_8.vcf.gz | sort -V > ${OUTPUT_PATH}/chr${SLURM_ARRAY_TASK_ID}/sort.temp
 bcftools concat --file-list ${OUTPUT_PATH}/chr${SLURM_ARRAY_TASK_ID}/sort.temp -Oz -o ${OUTPUT_PATH}/chr${SLURM_ARRAY_TASK_ID}/ukbb_wes_200k_chr${SLURM_ARRAY_TASK_ID}.vcf.bgz
-rm sort.temp
+rm ${OUTPUT_PATH}/chr${SLURM_ARRAY_TASK_ID}/sort.temp
