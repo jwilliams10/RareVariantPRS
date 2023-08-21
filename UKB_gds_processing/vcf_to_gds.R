2059a41c2cc616b1460f1feb6293e262bfcdb8ce
Jacob W <jacob.williams@nih.gov>
2023-08-21 08:26:40 -0400

    Updates

--- UKB_gds_processing/vcf_to_gds.R
+++ UKB_gds_processing/vcf_to_gds.R
@@ -5,5 +5,5 @@ print(chr)
 library(SeqArray)
 
 if(!file.exists(paste0("/data/williamsjacr/UKB_WES_lipids/Data/gds/all_chr",chr,".gds"))){
-  SeqArray::seqVCF2GDS(vcf.fn = paste0("/data/williamsjacr/UKB_WES_lipids/Data/pVCF/chr",chr,"/ukbb_wes_200k_chr",chr,".vcf.bgz"),out.fn = paste0("/data/williamsjacr/UKB_WES_lipids/Data/gds/all_chr",chr,".gds"),storage.option="LZMA_RA")  
+  SeqArray::seqVCF2GDS(paste0("/data/williamsjacr/UKB_WES_lipids/Data/pVCF/chr",chr,"/ukbb_wes_200k_chr",chr,".vcf.bgz"), out.fn = paste0("/data/williamsjacr/UKB_WES_lipids/Data/gds/all_chr",chr,".gds"), header = NULL, genotype.var.name = "GT", info.import=NULL, fmt.import=NULL, ignore.chr.prefix="chr", raise.error=TRUE, verbose=TRUE)
 }
