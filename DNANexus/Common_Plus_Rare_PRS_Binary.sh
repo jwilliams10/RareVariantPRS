00f1567a7e0f01c36cf8889cb787ae676ae619bb
Jacob W <jacob.williams@nih.gov>
2024-04-30 16:35:15 -0400

    Updates

--- DNANexus/Common_Plus_Rare_PRS_Binary.sh
+++ DNANexus/Common_Plus_Rare_PRS_Binary.sh
@@ -1,21 +1,25 @@
 # load bigsnpr image
 docker load -i r_with_plink.tar.gz
 
-for trait in Asthma CAD T2D Breast Prostate;
-do
-  dx download UKB_PRS:JW/UKB_Phenotypes/Results/Binary/Combined_Common_PRS/${trait}_Best_Tune_All.txt
-  dx download UKB_PRS:JW/UKB_Phenotypes/Results/Binary/Combined_Common_PRS/${trait}_Best_Validation_All.txt
-  
-  dx download UKB_PRS:JW/UKB_Phenotypes/Results/Binary/BestRareVariantPRS/${trait}_Best_All_STAARO_Tune_All.txt
-  dx download UKB_PRS:JW/UKB_Phenotypes/Results/Binary/BestRareVariantPRS/${trait}_Best_All_STAARO_Validation_All.txt
-  
-  dx download UKB_PRS:JW/UKB_Phenotypes/Results/Binary/BestRareVariantPRS/${trait}_Best_All_Burden_Tune_All.txt
-  dx download UKB_PRS:JW/UKB_Phenotypes/Results/Binary/BestRareVariantPRS/${trait}_Best_All_Burden_Validation_All.txt
-done
+if [ $1 = 1 ]
+then
+       trait=Asthma
+ elif [ $1 = 2 ]
+then
+       trait=CAD
+ elif [ $1 = 3 ]
+then
+       trait=T2D
+ elif [ $1 = 4 ]
+then 
+       trait=Breast
+else
+       trait=Prostate
+fi
 
-dx download UKB_PRS:JW/UKB_Phenotypes/Results/All_Tune.txt
-dx download UKB_PRS:JW/UKB_Phenotypes/Results/All_Validation.txt
+dx download UKB_PRS:JW/UKB_Phenotypes/Results/Binary/Combined_Common_PRS/${trait}_Best_Validation_All.txt
+dx download UKB_PRS:JW/UKB_Phenotypes/Results/Binary/BestRareVariantPRS/${trait}_BestPRS.csv
 dx download UKB_PRS:JW/UKB_Phenotypes/Results/all_phenotypes.RData
 
 # mount PWD and run bigsnpr using my_r_script.r
-docker run -v $PWD:/data -w /data --entrypoint /bin/bash r_with_plink -l -c "Rscript Common_Plus_Rare_PRS_Binary.R"
+docker run -v $PWD:/data -w /data --entrypoint /bin/bash r_with_plink -l -c "Rscript Single_RareVariant_PRS_All_Binary.R ${1}"
